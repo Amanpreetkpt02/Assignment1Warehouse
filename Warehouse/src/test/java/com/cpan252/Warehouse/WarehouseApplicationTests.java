ecc9e9887fb3aaab27e2ac86544f187bfa73a23f
Amanpreetkpt02 <amanpreetkpt02@gmail.com>
2023-02-17 12:11:36 -0500

    Dependencies added

---
+++ Warehouse/src/test/java/com/cpan252/Warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpan252.Warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
