8364774682b711cb0c94a69809db5eaed2b1dd25
Amanpreetkpt02 <amanpreetkpt02@gmail.com>
2023-02-17 23:44:09 -0500

    validation added

--- Warehouse/src/main/java/com/cpan252/Warehouse/model/Item.java
+++ Warehouse/src/main/java/com/cpan252/Warehouse/model/Item.java
@@ -1,5 +1,8 @@
 package com.cpan252.Warehouse.model;
 
+import java.util.Arrays;
+
+import jakarta.validation.constraints.Min;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,10 +12,22 @@ import lombok.Data;
 public class Item {
     private int id;
     private String name;
+    @Min(2021)
     private int yearofcreation;
+    @Min(1000)
     private double price;
     private FashionableBrand brand;
-
+    
+    public Item(int id, String name, int yearofcreation, double price, FashionableBrand brand){
+    if (!Arrays.asList(FashionableBrand.values()).contains(brand)) {
+        throw new IllegalArgumentException("Invalid brand name, must be one of: " + Arrays.asList(FashionableBrand.values()));
+      }
+      this.id = id;
+      this.name = name;
+      this.yearofcreation= yearofcreation;
+      this.price = price;
+      this.brand = brand;
+    }
     public enum FashionableBrand{
         BALENCIAGA("BALENCIAGA" ),STONE("STONE"),ISLAND("ISLAND"),DIOR("DIOR"), DOLCEGABBANA("DOLCEGABBANA");
 
